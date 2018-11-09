4fa0819c1f3d85ff4783bec2c6e96bfc9baabe22
yuyuko <ncysatnaf@gmail.com>
2018-11-09 14:06:15 +0800

    add cache

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const axios = require('axios');
+const cache = require("koa-redis-cache")
 
 var app = new Koa();
 var router = new Router();
 
-const { PORT = 8080 } = process.env;
+const { PORT = 8001 } = process.env;
 
 app.use(bodyParser());
+
+app.use(
+	cache({
+		redis: {
+			host: process.env.REDIS_HOST,
+			port: process.env.REDIS_PORT
+		},
+		// routes: ['/index'],
+		onerror(err) {
+			console.log(err);
+		}
+	})
+);
+
 
 app.use(async (ctx, next) => {
 	const start = new Date();
@@ -28,10 +43,16 @@ app.use(async (ctx, next) => {
 	}
 });
 
-router.post('/proxy', async (ctx, next) => {
+router.post('/axios', async (ctx, next) => {
 	let { data } = await axios(ctx.request.body);
 	ctx.body = data;
 });
+
+router.get("/", async( ctx, next) => {
+	const { url } = ctx.request.query
+	let { data } = await axios.get(url)
+	ctx.body = data;
+})
 
 app.use(router.routes());
 app.use(router.allowedMethods());
