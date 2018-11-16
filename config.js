3475a8708f55d8088301144879c0a4d702c8577a
yuyuko <ncysatnaf@gmail.com>
2018-11-16 20:03:57 +0800

    update config

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
   HOST: process.env.HOST || "localhost",
   PORT: process.env.PORT || 8001,
-  ES_ENABLE: process.env.ES_ENABLE || false,
-  REDIS_ENABLE: process.env.REDIS_ENABLE || true,
+  ES_ENABLE: process.env.ES_ENABLE ? process.env.ES_ENABLE == "ture" : false,
+  REDIS_ENABLE: process.env.REDIS_ENABLE ? process.env.REDIS_ENABLE == "ture" : true,
   REDIS_HOST: process.env.REDIS_HOST || "localhost",
   REDIS_PORT: process.env.REDIS_PORT || "6379",
-  MONGO_ENABLE: process.env.MONGO_ENABLE || true,
+  MONGO_ENABLE: process.env.MONGO_ENABLE ? process.env.REDIS_ENABLE == "ture" : true,
   MONGO_URL: process.env.MONGO_URL || "localhost:27017",
   MONGO_DATABASE: process.env.MONGO_URL || "test",
   JWT_SECRET: process.env.JWT_SECRET || "secret",
   JWT_EXP: process.env.JWT_EXP || Math.floor(Date.now() / 1000) + 60 * 60 * 12,
-  SOCKET_IO_ENABLE: process.env.SOCKET_IO_ENABLE || true,
-  SOCKET_IO_CLIENT_ENABLE: process.env.SOCKET_IO_CLIENT_ENABLE || true
+  SOCKET_IO_ENABLE: process.env.SOCKET_IO_ENABLE ? process.env.REDIS_ENABLE == "ture" : false,
+  SOCKET_IO_CLIENT_ENABLE: process.env.SOCKET_IO_CLIENT_ENABLE ? process.env.REDIS_ENABLE == "ture" : false
 };
