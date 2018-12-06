770b5d600fa81269aed6f90c98e6750603624ec0
yuyuko <ncysatnaf@gmail.com>
2018-12-06 19:23:38 +0800

    add graphql example

---
+++ examples/graphql/app.js
@@ -0,0 +1,20 @@
+const { koma } = require("../../index");
+const path = require("path");
+
+koma.$use({
+  start: {
+    metas: {
+      mongoose: { load: true },
+      graphql: { load: true },
+      jwt: { load: true }
+    },
+    load: {
+      plugins: [],
+      modules: ["modules"].map(i => path.join(__dirname, i))
+    },
+    config: {
+      PORT: 8001,
+      RUN: true
+    }
+  }
+});
---
+++ examples/graphql/modules/account.js
@@ -0,0 +1,52 @@
+const _ = require("lodash");
+const { MongooseUtils } = require("../../../plugins/mongoose");
+const { models } = MongooseUtils;
+const { signJWT } = require("../../../plugins/jwt");
+
+module.exports = {
+  name: "User",
+  gql: {
+    Mutation: {
+      Login: {
+        type: `type Login {account: Account!, jwt: String!}`,
+        args: { identifier: "String!", password: "String!" },
+        resolve: async ({ args }) => {
+          const { identifier, password } = args;
+          const account = await models("Account")
+            .findOne({ $or: [{ username: identifier }] })
+            .select("+password");
+          if (!account) throw new Error("用户不存在");
+          const validPassword = await account.verifyPassword(password);
+          if (validPassword) {
+            delete account.password;
+            return { account, jwt: signJWT({ _id: account._id, role: account.role }) };
+          } else {
+            throw new Error("用户名或密码错误");
+          }
+        }
+      },
+      SignUp: {
+        type: `type User {username: String!}`,
+        args: {
+          username: "String!",
+          password: "String!"
+        },
+        resolve: async ({ args }) => {
+          const user = await models("Account").create(args);
+          return user;
+        }
+      }
+    }
+  },
+  models: {
+    Account: {
+      schema: {
+        username: { type: "string", required: true, unique: true },
+        password: { type: "string", select: false, required: true, bcrypt: true, hidden: true }
+      },
+      options: {
+        timestamp: true
+      }
+    }
+  }
+};
